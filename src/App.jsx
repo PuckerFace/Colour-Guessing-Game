cc51b4726ce24b43bbcac0c7df907e82320b4811
Bukola A <bumolaya@gmail.com>
2025-02-03 11:01:06 -0800

    major changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,51 @@
 import { useEffect, useState } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
 
 const App = () => {
-  const colours = ['purple', 'green', 'yellow', 'red', 'blue', 'pink'];
-  // const colours = [
-  //   '#5f4e91',
-  //   '#c8b6ff',
-  //   '#937adc',
-  //   '#9370DB',
-  //   '#806ac3',
-  //   '#8c69f5',
-  // ];
+  const colours = [
+    '#FFBF00',
+    '#DE3163 ',
+    '#9FE2BF',
+    '#6495ED',
+    '#FF7F50',
+    '#CCCCFF',
+  ];
+
   const [score, setScore] = useState(0);
   const [message, setMessage] = useState('Guess the correct colour!');
   const [targetColour, setTargetColour] = useState('');
   const [isCorrect, setIsCorrect] = useState(null);
-  const [rounds, setRounds] = useState(0);
-  const [highScore, setHighScore] = useState(0);
   const [animation, setAnimation] = useState('');
-  const [gameOver, setGameOver] = useState(false);
+
+  useEffect(() => {
+    colourGame();
+  }, []);
 
   const colourGame = () => {
     const randomColor = colours[Math.floor(Math.random() * colours.length)];
     setTargetColour(randomColor);
     setMessage('Guess the correct colour!');
     setIsCorrect(null);
-    setGameOver(false);
-    setRounds(0);
     setAnimation('');
-    setScore(0);
-    setHighScore(Math.max(highScore, score));
   };
-  const handleGame = (selectedColour) => {
-    if (gameOver) return;
 
+  const handleGame = (selectedColour) => {
     if (selectedColour === targetColour) {
       setMessage('Colour Guessed Correctly');
       setScore((score) => score + 1);
       setIsCorrect(true);
       setAnimation('bounce');
-      toast.success('Correct!');
+      const randomColor = colours[Math.floor(Math.random() * colours.length)];
+      setTargetColour(randomColor);
     } else {
       setMessage('Try Again!, Guess the correct colour!');
       setIsCorrect(false);
       setAnimation('fade-out');
-      toast.error('Wrong!');
     }
 
     setTimeout(() => {
       setAnimation('');
     }, 500);
-
-    setRounds((prevRounds) => prevRounds + 1);
   };
-
-  useEffect(() => {
-    colourGame();
-  }, []);
-
-  useEffect(() => {
-    if (rounds === 5) {
-      setGameOver(true);
-      setMessage('Game over! Restart to play again.');
-      setHighScore((prevHighScore) => Math.max(prevHighScore, score));
-
-      if (score >= 5) {
-        toast.success('Game Over! You won!');
-      } else {
-        toast.error('Game Over! You lost!');
-      }
-    } else if (rounds < 5) {
-      // Set a new target colour if the game is still ongoing
-      const randomColour = colours[Math.floor(Math.random() * colours.length)];
-      setTargetColour(randomColour);
-    }
-  }, [rounds, score]); // Run when `newRounds` or `score` changes
 
   return (
     <section>
@@ -98,10 +69,6 @@ const App = () => {
                   : 'Make a guess!'}
               </p>
             </div>
-
-            <div className="round-box">
-              <p data-testid="round">Rounds: {rounds}/5</p>
-            </div>
           </div>
           <div className="row">
             <div className="score-box">
@@ -110,9 +77,6 @@ const App = () => {
                 Score:
                 {score}
               </p>
-            </div>
-            <div className="highScore-box">
-              <p data-testid="highScore">High Score: {highScore}</p>
             </div>
           </div>
         </div>
@@ -129,20 +93,20 @@ const App = () => {
                 key={index}
                 style={{ backgroundColor: colour }}
                 onClick={() => handleGame(colour)}
-                className="color-option"
+                // className="color-option"
+                className={`color-option ${animation}`}
               ></button>
             ))}
           </div>
           <div className="new-game-box">
             <button
               data-testid="newGameButton"
-              className={`new-game ${gameOver ? 'bounce' : ''}`}
+              className="new-game"
               onClick={colourGame}
             >
               New Game
             </button>
           </div>
-          <ToastContainer autoClose={1000} />
         </div>
       </div>
     </section>
